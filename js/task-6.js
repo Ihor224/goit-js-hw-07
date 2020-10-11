329836307f0bfb10f4905edb7f68d093dc16a42f
Ihor Levchenko <ihor224@gmail.com>
2020-10-11 11:21:50 +0300

    Сделал правки

--- js/task-6.js
+++ js/task-6.js
@@ -4,8 +4,17 @@ const refs = {
 
 refs.input.addEventListener("blur", onInputBlur);
 
+// function onInputBlur() {
+//   refs.input.value.length !== Number(refs.input.dataset.length)
+//     ? refs.input.classList.add("invalid")
+//     : refs.input.classList.replace("invalid", "valid");
+// }
+
 function onInputBlur() {
-  refs.input.value.length !== Number(refs.input.dataset.length)
-    ? refs.input.classList.add("invalid")
-    : refs.input.classList.replace("invalid", "valid");
+  if (refs.input.value.length !== Number(refs.input.dataset.length)) {
+    refs.input.classList.add("invalid");
+  } else if (refs.input.value.length === Number(refs.input.dataset.length)) {
+    refs.input.classList.add("valid");
+    refs.input.classList.remove("invalid");
+  }
 }
